1dc16bbb58315fb4a82bef213a0eb79f85033751
L1MJH <37625953+LimJH2002@users.noreply.github.com>
2023-10-24 00:13:43 +0800

    Add Hero Section

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
+import React from "react";
 import "./App.css";
-import logo from "./logo.svg";
+import Hero from "./components/Hero";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <h1 className="text-3xl font-bold underline">Hello world!</h1>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App bg-gray-800 text-white">
+      <Navbar />
+      <Hero />
     </div>
   );
 }
