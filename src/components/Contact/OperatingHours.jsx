96345cf2ee1c16e9a7a0e98b42390f1a15a0ae7e
L1MJH <37625953+LimJH2002@users.noreply.github.com>
2023-10-24 19:19:43 +0800

    Add contact info

---
+++ src/components/Contact/OperatingHours.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+const body = () => {
+  return (
+    <div className="mx-auto max-w-7xl px-6 pt-12 pb-6 lg:px-8">
+      <div className="">
+        <div className="">
+          <h2 className="text-2xl font-bold text-gray-900 sm:text-3xl sm:tracking-tight">
+            Operating Hours
+          </h2>
+          <div className="mt-4">
+            <div className="ml-3 text-base text-gray-500">
+              <p className="mt-1">Mon-Fri 9am to 6pm</p>
+              <p className="mt-1">
+                Closed on Saturday, Sunday & Public Holidays
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default body;
